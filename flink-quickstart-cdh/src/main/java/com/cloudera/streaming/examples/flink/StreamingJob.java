0c2168cf9b9e1db89ffe3a14f99acd30c9d9ea9f
Matyas Orhidi <morhidi@cloudera.com>
2019-07-29 17:15:04 +0200

    added foo bar print

--- flink-quickstart-cdh/src/main/java/com/cloudera/streaming/examples/flink/StreamingJob.java
+++ flink-quickstart-cdh/src/main/java/com/cloudera/streaming/examples/flink/StreamingJob.java
@@ -18,6 +18,8 @@
 
 package com.cloudera.streaming.examples.flink;
 
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 /**
@@ -38,27 +40,9 @@ public class StreamingJob {
 		// set up the streaming execution environment
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+		DataStream<String> stream = env.fromElements("foo", "bar");
 
-		// execute program
+		stream.print();
 		env.execute("Flink Streaming Java API Skeleton");
 	}
 }
